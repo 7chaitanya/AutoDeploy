d3b143a00b3c0cc79020e9fbba6bcd8461561acd
Verma <chaitanya.verma@innovasolutions.com>
2025-09-30 16:28:02 +0530

    hi

---
+++ src/index.js
@@ -0,0 +1,8 @@
+exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Demo Service is running" }),
+  };
+};
+/*hi*/
+console.log('hihihihihihiji chaitanya here');
