f2bf69d78187621b883870676d71c4f8d64e91ea
Verma <chaitanya.verma@innovasolutions.com>
2025-10-01 12:31:52 +0530

    rollback added

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,15 +2,26 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Lambda Function with Versioning Enabled
 resource "aws_lambda_function" "demo_service" {
-  function_name = "demo-service-e"
-  filename      = "auto-deploy.zip"
-  handler       = "index.handler"
-  runtime       = "nodejs18.x"
-  role          = "arn:aws:iam::612572392212:role/AWSLambdaExecutionRole"
+  function_name    = "demo-service-e"
+  filename         = "auto-deploy.zip"
+  handler          = "index.handler"
+  runtime          = "nodejs18.x"
+  role             = "arn:aws:iam::612572392212:role/AWSLambdaExecutionRole"
   source_code_hash = filebase64sha256("auto-deploy.zip")
+
+  publish = true # <-- ensures new version is published on each deploy
 }
 
+# Lambda Alias pointing to the latest stable version
+resource "aws_lambda_alias" "live" {
+  name             = "live"
+  function_name    = aws_lambda_function.demo_service.function_name
+  function_version = aws_lambda_function.demo_service.version
+}
+
+# CloudWatch Alarm for Lambda Errors
 resource "aws_cloudwatch_metric_alarm" "lambda_errors" {
   alarm_name          = "DemoServiceErrors"
   comparison_operator = "GreaterThanThreshold"
